60d38aed54a2c7c7216e26d8e8e022902708239a
Nathan Lardizabal <nathanjlardizabal@gmail.com>
2020-05-05 17:07:28 -0400

    change initial text

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const vision = 
+    "We are a Christ-centred, Holy Spirit-led community committed to " +
+    "the building of strong relationships and planting of healthy " +
+    "reproducing churches.";
+  const mission =
+    "to introduce and represent Christ through our nurturing small " +
+    "group-driven ministries in the community.";
+
   return (
     <div>
-      This is a sample stateful and server-side
-      rendered React application.
+      <h1 className="title">Welcome to Freedom in Christ Church!</h1>
+      <h2 className="vision">{vision}</h2>
+      <h3 className="mission">{mission}</h3>
     </div>
   );
 }
