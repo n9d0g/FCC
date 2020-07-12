474805c79c9318be9ec2bf04d186643dabd8c2a5
Nathan Lardizabal <nathanjlardizabal@gmail.com>
2020-07-12 01:14:11 -0400

    okay looking landing page

--- src/modules/home/home-main/HomeMainView.tsx
+++ src/modules/home/home-main/HomeMainView.tsx
@@ -5,25 +5,29 @@ import './HomeMainView.css'
 
 const HomeMainView = () => {
   return (
-    <section className='home-landingpage'>
-      <div className='home-welcome'>
-        <div className='home-welcome-content'>
-          <h1 className='church-name'>Freedom in Christ Church</h1>
-          <div className='home-welcome-statement'>
-            <div className='home-welcome-title-container'>
-              <h1 className='title-small'>To Proclaim and Display the</h1>
-              Reconciling Work of Christ
-            </div>
-
-            <div className='newButton'>
-              <Link to='/about'>
-                <HomeButton text="I'm new here" />
-              </Link>
-              <Link to='/sermons'>
-                <HomeButton text='Sermons' />
-              </Link>
-            </div>
+    <section className='home-welcome'>
+      <div className='home-welcome-content'>
+        <h1 className='church-name'>Freedom in Christ Church</h1>
+        <div className='home-welcome-statement'>
+          <div className='home-welcome-title-container'>
+            <h1 className='title-small'>To Proclaim and Display the</h1>
+            <h2 className='title-big'>Reconciling</h2>
+            <h2 className='title-big'>Work of Christ</h2>
           </div>
+          <div className='home-welcome-buttons'>
+            <Link to='/about'>
+              <HomeButton text="I'm new here" />
+            </Link>
+            <Link to='/sermons'>
+              <HomeButton text='Sermons' />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='home-welcome-invitation'>
+        <div className='empty-box'></div>
+        <div className='home-welcome-invitation-text'>
+          Join us via ZOOM on Sundays at 10:30am.{' '}
         </div>
       </div>
     </section>
