21ebab12c9297c08c7708a100960e38506ab57ad
Nathan Lardizabal <nathanjlardizabal@gmail.com>
2021-01-13 09:48:25 -0500

    updated ministries page

---
+++ src/modules/connect/components/Ministry.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+
+const Ministry: FC = () => {
+    return (
+        <div className="ministries-content-ministry">
+            <div className="ministries-content-ministry-description-container">
+                <h2 className="ministries-content-ministry-description-title">
+                    FCC YA
+                </h2>
+                <p className="ministries-content-ministry-description-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>Contact: Bro. Nathan Lardizabal (123-123-1234)</p>
+            </div>
+        </div>
+    )
+}
+
+export default Ministry
--- src/modules/connect/connect-ministries/ConnectMinistriesView.tsx
+++ src/modules/connect/connect-ministries/ConnectMinistriesView.tsx
@@ -1,24 +1,60 @@
-import React, {FC, useEffect} from 'react'
+import React, { FC, useEffect } from 'react'
 import TitleAndBreadcrumb from '../../../components/title-and-breadcrumb/TitleAndBreadcrumb'
+import Ministry from '../components/Ministry'
+import './ConnectMinistriesView.css'
+
+// ministries:
+// 1. FCC YA
+// 2. FCC youth
+// 3. FCC children
+// 4. praise and worship ministry
 
 const ConnectMinistriesView: FC = () => {
-  useEffect(() => {
-    document.title = 'Ministries | FCC'
-    return () => {
-      console.log('unmount')
-    }
-  }, [])
+    useEffect(() => {
+        document.title = 'Ministries | FCC'
+        return () => {
+            console.log('unmount')
+        }
+    }, [])
 
-  return (
-    <div className='content-container'>
-      <div className='connect-subtitle-container'>
-        <TitleAndBreadcrumb
-          title='Our Ministries'
-          secondBreadcrumbTitle='Connect'
-        />
-      </div>
-    </div>
-  )
+    return (
+        <div className="ministries-container">
+            <div className="title-container">
+                <TitleAndBreadcrumb
+                    title="Our Ministries"
+                    secondBreadcrumbTitle="Connect"
+                />
+                <div className="ministries-title-picture-container">
+                    <div className="ministries-title-picture"></div>
+                    <div className="ministries-title-description">
+                        <p className="ministries-title-description-text">
+                            God created all of us with an inherent need for
+                            community. It is in the context of meaningful
+                            relationships with other believers that we grow as
+                            disciples of Jesus Christ (Hebrews 10:24-25).
+                            Connection Groups are the way we establish this kind
+                            of biblical community.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="ministries-pre-content-container">
+                <div className="ministries-pre-content-title-container">
+                    <h2 className="ministries-pre-content-title">
+                        Connect with one of our Ministries!
+                    </h2>
+                </div>
+            </div>
+            <div className="ministries-content-container">
+                <div className="ministries-content-ministries-container">
+                    <Ministry />
+                    <Ministry />
+                    <Ministry />
+                    <Ministry />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ConnectMinistriesView
